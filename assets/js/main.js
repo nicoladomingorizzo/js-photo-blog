8b56bd98ac01393a9e5d5c2b6dc1cab2ef9cd3d3
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-17 12:51:15 +0200

    Refactor CSS styles for consistency, add responsive design, and update HTML structure; include new image asset

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come po
 Milestone 3
 Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
 Font utilizzati:
-titoli: ‘Edu Tas Beginner’, sans-serif;d ate: ‘Sometype Mono’, ‘monospace’; (Dovreste sapere a questo punto cosa e come prendere da Google Fonts…
+titoli: ‘Edu Tas Beginner’, sans-serif; date: ‘Sometype Mono’, ‘monospace’; (Dovreste sapere a questo punto cosa e come prendere da Google Fonts…
 */
 
 /*
