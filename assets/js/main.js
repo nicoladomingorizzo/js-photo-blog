ebb787210493ead29025e088c0a0d44f77fe4f17
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-18 10:23:25 +0200

    Add overlay functionality with modal for image display; improve CSS structure and responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,7 @@ titoli: ‘Edu Tas Beginner’, sans-serif; date: ‘Sometype Mono’, ‘monosp
 
 const imgApiUrl = 'https://lanciweb.github.io/demo/api/pictures/?id=6';
 const imgContainerEl = document.getElementById('imgContainer');
+const overlayEl = document.getElementById('overlay')
 
 for (let i = 0; i < 6; i++) {
     fetch(imgApiUrl)
@@ -43,7 +44,7 @@ for (let i = 0; i < 6; i++) {
                 </div>
                 <div class="card-body">
                     <p>${data[i].title}</p>
-                    <p>${data[i].date}</p>
+                    <p>${data[i].date}</p> 
                 </div>
                 </div>`;
             imgContainerEl.appendChild(cardEl);
@@ -53,10 +54,17 @@ for (let i = 0; i < 6; i++) {
 
 
 /*
+CONSEGNA
+Milestone 1
+Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, disponiamo un’immagine qualunque ed un button di chiusura.
+Milestone 2
+Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+Milestone 3
+Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
+Ci sono diversi modi di farlo, prova a sperimentare :faccia_leggermente_sorridente:
 Bonus
-rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
-Note
-Non siete obbligati a usare Bootstrap: siete liberi di decidere come gestire lo stile
+Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta, il tutto in manierà fluida.
+Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
 */
-
-
